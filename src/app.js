b6d4ef762cddd1b09c3a17b170e6213eb094d98c
Jorgesoibz <jorgesoibz@gmail.com>
2025-04-07 18:51:47 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+    let who = ['El perro', 'Mi abuela', 'El ladron', 'Mi pajaro'];
+    let action = ['se comio', 'tiro', 'robo', 'perdio'];
+    let what = ['los deberes', 'mi cartera', 'el coche','el ordenador'];
+    let when = ['antes de clase', 'mientras dormia', 'cuando hacia ejercicio', 'cuando estaba comiendo,', 'durante mi rezo' ];
+    
+    
+      function generatorExcuses () {
+        let randomwho= who[Math.floor(Math.random()*who.length)]
+        let randomaction= action[Math.floor(Math.random()*action.length)]
+        let randomwhat= what[Math.floor(Math.random()*what.length)]
+        let randomwhen= when[Math.floor(Math.random()*when.length)]
+    
+        return `${randomwho} ${randomaction} ${randomwhat} ${randomwhen}`;
+      }
+    
+    
+    
+    document.getElementById('excuse').innerHTML = generatorExcuses() }
+;
